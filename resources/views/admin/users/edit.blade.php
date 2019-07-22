f33f188304414f04a3e3dff0c21315041e26d0d4
Aashir Haque <aashirulhaque@gmail.com>
2019-07-23 00:43:14 +0500

    folder 30 completed

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -50,12 +50,21 @@
 
 
             <div class="form-group">
-                {!! Form::submit('Update User', ['class'=>'btn btn-success']) !!}
+                {!! Form::submit('Update User', ['class'=>'btn btn-success col-sm-6']) !!}
+            </div>
+
+            {!! Form::close() !!}
+
+            {!! Form::open(['method'=>'DELETE', 'action'=>['AdminUsersController@destroy', $user->id]]) !!}
+
+            <div class="form-group">
+                {!! Form::submit('Delete', ['class'=>'btn btn-danger col-sm-6']) !!}
             </div>
 
             {!! Form::close() !!}
 
         </div>
+
 
     </div>
 
